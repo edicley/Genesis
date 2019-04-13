bd46a2a02bd0f60d122513e766ba7c9cd24133bf
Edicley Henrique <edyhenrique@gmail.com>
2019-04-13 09:06:25 -0300

    Verificando se o número informado é válido.

--- scr/Adivinhacao.py
+++ scr/Adivinhacao.py
@@ -4,20 +4,32 @@ print('*******************************')
 print('*     Jogo da adivinhação     *')
 print('*******************************')
 
+print('Tenta acertar o número que eu escolhi.')
+
 numero = random.randint(1,20)
 chance = 1
-
+tentativa = 3
+print(numero)
 while(chance <= 3):
 
-    print('Tenta acertar o número que eu escolhi.')
 
+    print('Tentativa {}  de {}.'.format(chance, tentativa))
+    print('Escolha um número entre 1 e 20.')
     chute = int(input('Escreva seu número: '))
+    chance = chance + 1
+
+    if(chute < 1 or chute > 20):
+        print('Número inválido. Você desperdiçou um chance.')
+        continue
 
     if(chute == numero):
         print('Você acertou.')
+        break
     elif(chute < numero):
         print('Você errou. O numero que eu escolhi é maior que', chute)
     elif(chute > numero):
         print('Você errou. O número que eu escolhi é menor que', chute)
 
-    chance = chance + 1
+
+
+print('Fim de Jogo!')
