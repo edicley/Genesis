1cfdc5127bb01db8cd5daac3b7d113c46c3d9c7f
Edicley Henrique <edyhenrique@gmail.com>
2019-04-13 10:48:53 -0300

    Criando função jogar.

--- scr/Adivinhacao.py
+++ scr/Adivinhacao.py
@@ -1,48 +1,51 @@
 import random
 
-print('*******************************')
-print('*     Jogo da adivinhação     *')
-print('*******************************')
+def jogar():
 
-print('Tenta acertar o número que eu escolhi.')
+    print('*******************************')
+    print('*     Jogo da adivinhação     *')
+    print('*******************************')
 
-numero = random.randint(1,100)
-chance = 1
-tentativa = 3
+    print('Tenta acertar o número que eu escolhi.')
 
-print('Escolha o nível que quer jogar.')
-print('[1]-Fácil [2]-Médio [3]-Difícil')
+    numero = random.randint( 1,100 )
+    chance = 1
+    tentativa = 0
 
-nivel = int(input('Selecione o nível:'))
+    print('Escolha o nível que quer jogar.')
+    print('[1]-Fácil [2]-Médio [3]-Difícil')
 
-if(nivel == 1):
-    tentativa = 15
-elif(nivel == 2):
-    tentativa = 10
-else:
-    tentativa = 5
+    nivel = int(input('Selecione o nível:'))
+    
+    if(nivel == 1):
+        tentativa = 15
+    elif(nivel == 2):
+        tentativa = 10
+    else:
+        tentativa = 5
 
-print(numero)
-while(chance <= tentativa):
-
-
-    print('Tentativa {}  de {}.'.format(chance, tentativa))
-    print('Escolha um número entre 1 e 20.')
-    chute = int(input('Escreva seu número: '))
-    chance = chance + 1
-
-    if(chute < 1 or chute > 100):
-        print('Número inválido. Você desperdiçou um chance.')
-        continue
-
-    if(chute == numero):
-        print('Você acertou.')
-        break
-    elif(chute < numero):
-        print('Você errou. O numero que eu escolhi é maior que', chute)
-    elif(chute > numero):
-        print('Você errou. O número que eu escolhi é menor que', chute)
+    #print(numero)
+    while(chance <= tentativa):
 
 
+        print('Tentativa {}  de {}.'.format(chance, tentativa))
+        print('Escolha um número entre 1 e 100.')
 
-print('Fim de Jogo!')
+        chute = int(input('Escreva seu número: '))
+        chance = chance + 1
+
+        if(chute < 1 or chute > 100):
+            print('Número inválido. Você desperdiçou um chance.')
+            continue
+
+        if(chute == numero):
+            print('Você acertou.')
+            break
+        elif(chute < numero):
+            print('Você errou. O numero que eu escolhi é maior que', chute)
+        elif(chute > numero):
+            print('Você errou. O número que eu escolhi é menor que', chute)
+
+
+    print('Eu escolhi o número', numero)
+    print('Fim de Jogo!')
