d4bc98a791d56d7e938bbdbb9bd286757ee6b491
Edicley Henrique <edyhenrique@gmail.com>
2019-04-12 21:50:16 -0300

    Criando variáveis e testando condições.

--- scr/Adivinhacao.py
+++ scr/Adivinhacao.py
@@ -1,5 +1,17 @@
 
 
 print('*******************************')
-print('      Jogo da adivinhação      ')
+print('*     Jogo da adivinhação     *')
 print('*******************************')
+
+numero = 5
+
+print('Tenta acertar o número que eu escolhi.')
+
+chute = int(input('Escreva seu número: '))
+
+if(chute == numero):
+    print('Você acertou.')
+else:
+    print('Você errou.')
+
